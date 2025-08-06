2da5a3e273d1f31fb98034175cc007b9da85e3d2
xyzzzvy <121162251+xyzzzvy@users.noreply.github.com>
2025-08-06 17:37:29 +0200

    patch

--- Admin/Js/logout.js
+++ Admin/Js/logout.js
@@ -11,5 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = "Bestellungen.html";
     })
 
+    const btn = document.getElementById("register");
+    if (btn) {
+        btn.addEventListener("click", () => {
+            window.location.href = "register-page.html";
+        });
+    }
 
 })
--- Admin/Js/swappagestoregister.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const apiBase = window.location.origin;
-    const btn = document.getElementById("register");
-    if (btn) {
-        btn.addEventListener("click", () => {
-            window.location.href = apiBase + "/Admin/register-page.html";
-        });
-    }
-});
