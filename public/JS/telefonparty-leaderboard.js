827f9a593162864362bd4005ea783de3f87e6479
uzbek5000 <defnotasecacc@gmail.com>
2025-09-25 21:02:08 +0200

    first version leaderboard

---
+++ public/JS/telefonparty-leaderboard.js
@@ -0,0 +1,43 @@
+function renderLeaderboard(rawData) {
+    let preparedData = prepareData(rawData);
+    const leaderboard = document.getElementById('leaderboard');
+
+    if(preparedData.length === 0) {
+        console.log('No data found');
+        return;
+    }
+
+    preparedData.forEach((user, index) => {
+        const entry = document.createElement('div');
+        entry.classList.add('leaderboard-entry');
+
+        const rankDiv = document.createElement('div');
+        rankDiv.classList.add('rank');
+        rankDiv.textContent = index + 1;
+
+        const nameDiv = document.createElement('div');
+        nameDiv.classList.add('entry-value');
+        nameDiv.textContent = user.name;
+
+        const pointsDiv = document.createElement('div');
+        pointsDiv.classList.add('entry-value');
+        pointsDiv.textContent = user.points;
+
+        entry.appendChild(rankDiv);
+        entry.appendChild(nameDiv);
+        entry.appendChild(pointsDiv);
+    })
+}
+
+function prepareData(rawData) {
+    if (!rawData || !Array.isArray(rawData)) return [];
+
+    const users = rawData.users.map(user => ({
+        name: user.name,
+        points: user.termineneu
+    }));
+
+    users.sort((a, b) => b.points - a.points);
+
+    return users;
+}
